e879628f85d2552afdf43d7fa614d12606be63ab
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-28 12:23:17 +0500

    feat: mount video routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
-
+import videoRoutes from './routes/videoRoutes.js'
 
 const app=express();
 
-app.use('/',(req,res)=>{
-    return res.status(200).json({
-        success:true,
-        message:'tubely api is running',
-    })
-})
+app.use('/api/v1/videos',videoRoutes);
+
 const PORT=process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`visit http://localhost:${PORT}`);
